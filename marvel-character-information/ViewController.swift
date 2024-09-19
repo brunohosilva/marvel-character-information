31e5b07534fc72f02b7d047697a0af28e12e45b2
Bruno Oliveira <brunohosilva97@gmail.com>
2024-09-19 15:52:56 -0300

    Initial Commit

---
+++ marvel-character-information/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  marvel-character-information
+//
+//  Created by Bruno Oliveira on 19/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

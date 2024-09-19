7c627fcd512a93c2df63b209b58ade4e1e616c78
Bruno Oliveira <brunohosilva97@gmail.com>
2024-09-19 16:31:30 -0300

    setup to viewcode mode

--- marvel-character-information/ViewController.swift
+++ marvel-character-information/ViewController.swift
@@ -8,13 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.text = "Hello, ViewCode!"
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-            print("teste")
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        // Adicionando a label na hierarquia da view
+        view.addSubview(label)
+        
+        // Definir as constraints da label
+        setupConstraints()
     }
-
-
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
 }
+
 
